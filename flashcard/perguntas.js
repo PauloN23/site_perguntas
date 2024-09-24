fd6c79fe6aba4a52b131c65925b6250196bc9998
PauloN23 <paulo.luizdasilva.neto@escola.pr.gov.br>
2024-09-24 12:07:27 -0300

    .

---
+++ flashcard/perguntas.js
@@ -0,0 +1,27 @@
+criaCartao(
+    'DragoBall',
+    'Qual o primeiro vilão da saga de DragonBall?',
+    'O primeiro vilão da saga de Dragonball foi Oolong'
+)
+
+criaCartao(
+    'DragonBall',
+    'Qual a primeira técnica usada pelo Goku?',
+    'A primeira técnica usada pelo Goku foi o Kamehameha, onde Goku aprendeu apenas vendo o Mestre Kame usando na saga de DragonBall'
+)
+
+criaCartao(
+    'DragonBall Z',
+    'Qual são as sagas de DragonBall Z?, tirando a saga de Garlic JR e a Saga do torneio do outro mundo',
+    'São as sagas de DragonBall Z, Saga dos Saiyajins, Saga de Freeza, Saga dos Androides, Saga de Cell, Saga de Majin Boo'
+)
+
+criaCartao(
+    'DragonBall Z',
+    'Qual técnica Goku usa para derrotar Vegeta?',
+    'Goku usou a técnica kaoiken para derrotar Vegeta, onde ele aprendeu com o senhor Kaioh'
+)
+
+criaCartao(
+    
+)
