258cac1bcaf8fef970834f11ffb63b0ccf75e5f5
PauloN23 <paulo.luizdasilva.neto@escola.pr.gov.br>
2024-10-02 21:34:03 -0300

    .

--- flashcard/perguntas.js
+++ flashcard/perguntas.js
@@ -1,23 +1,35 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Harry Potter',
+    'Qual é o nome do diretor da Escola de Magia e Bruxaria de Hogwarts durante a maior parte da série?',
+    'O nome do diretor é Alvo Dumbledore.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Harry Potter',
+    'Qual é o nome do vilão principal da série?',
+    'O vilão principal é Lord Voldemort, também conhecido como Tom Riddle.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Harry Potter',
+    'Quais são os nomes dos melhores amigos de Harry Potter?',
+    'Os melhores amigos de Harry são Hermione Granger e Rony Weasley.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Harry Potter',
+    'Qual é a função do "Expecto Patronum"?',
+    '"Expecto Patronum" é um feitiço que evoca um Patrono, uma forma mágica que protege o bruxo contra Dementadores.'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o feitiço usado para desarmar um oponente?',
+    'O feitiço usado para desarmar um oponente é "Expelliarmus".'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Qual é o nome do elfo doméstico que serve a família Malfoy?',
+    'O elfo doméstico é chamado Dobby.".'
 )
