78aa6dfc7ce5e8b37f8bc9cd12187109ac07c682
PauloN23 <paulo.luizdasilva.neto@escola.pr.gov.br>
2024-11-04 10:12:39 -0300

    b

--- flashcard/perguntas.js
+++ flashcard/perguntas.js
@@ -1,35 +1,23 @@
 criaCartao(
-    'Harry Potter',
-    'Qual é o nome do diretor da Escola de Magia e Bruxaria de Hogwarts durante a maior parte da série?',
-    'O nome do diretor é Alvo Dumbledore.'
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    'Harry Potter',
-    'Qual é o nome do vilão principal da série?',
-    'O vilão principal é Lord Voldemort, também conhecido como Tom Riddle.'
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
 )
 
 criaCartao(
-    'Harry Potter',
-    'Quais são os nomes dos melhores amigos de Harry Potter?',
-    'Os melhores amigos de Harry são Hermione Granger e Rony Weasley.'
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Harry Potter',
-    'Qual é a função do "Expecto Patronum"?',
-    '"Expecto Patronum" é um feitiço que evoca um Patrono, uma forma mágica que protege o bruxo contra Dementadores.'
-)
-
-criaCartao(
-    'Harry Potter',
-    'Qual é o feitiço usado para desarmar um oponente?',
-    'O feitiço usado para desarmar um oponente é "Expelliarmus".'
-)
-
-criaCartao(
-    'Harry Potter',
-    'Qual é o nome do elfo doméstico que serve a família Malfoy?',
-    'O elfo doméstico é chamado Dobby.".'
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
